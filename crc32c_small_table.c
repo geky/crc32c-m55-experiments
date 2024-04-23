a1e6ee28199049cb45a5337bcdfa0d2ececb3dff
Christopher Haster <geky@geky.net>
2024-04-23 17:41:32 -0500

    Fixed overallocation of small-table array
    Whoops. An unfortunate typo that went unnoticed. This should be a
    64-byte (16-int) array, not a 64-int array.
    
    A free 192 byte savings!
    
    Fortunately this does not effect instruction counts or RAM usage, only
    the resulting code size.
    
    Results remeasured and README.md updated

--- crc32c_small_table.c
+++ crc32c_small_table.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <stddef.h>
 
-static const uint32_t TABLE[64] = {
+static const uint32_t TABLE[16] = {
     0x00000000, 0x105ec76f, 0x20bd8ede, 0x30e349b1,
     0x417b1dbc, 0x5125dad3, 0x61c69362, 0x7198540d,
     0x82f63b78, 0x92a8fc17, 0xa24bb5a6, 0xb21572c9,
